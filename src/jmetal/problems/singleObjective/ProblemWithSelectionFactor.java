a416748b53bc2b36d65f4553b69a30c6bdd73054
Italo <italo@iMac-de-Laconi.local>
2017-05-19 16:54:02 -0300

    first commit

---
+++ src/jmetal/problems/singleObjective/ProblemWithSelectionFactor.java
@@ -0,0 +1,16 @@
+package jmetal.problems.singleObjective;
+
+import jmetal.core.Problem;
+import jmetal.core.Solution;
+
+/**
+ * Created by italo on 19/05/17.
+ */
+public abstract class ProblemWithSelectionFactor extends Problem{
+    
+
+    @Override
+    public void evaluate(Solution solution) {
+
+    }
+}
